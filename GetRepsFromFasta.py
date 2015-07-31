373fde52b32770437e2f3b18c599f98f7b46669f
bryesk <Bry@nagini-250.dynamic.rpi.edu>
2015-07-31 14:17:10 -0400

    GetRepsFromFasta
    Fixes error where if no genus names was found the program terminated.
    Now if no name is found it skips that sequence. Also, moved some
    function to the dante.py module.

--- GetRepsFromFasta.py
+++ GetRepsFromFasta.py
@@ -11,28 +11,20 @@ import sys
 import re
 #import traceback to help with exceptions
 import traceback
+#import dante functions
+import dante
 
 try: 
-
-    #Function for removing the directory of a file name
-    def getFilename(whole_name):
-        reversed_name = whole_name[::-1]
-        trun_rev=""
-        for i in range(len(whole_name)):
-            if reversed_name[i] == "/":
-                break
-            else:
-                trun_rev = trun_rev + reversed_name[i]
-        new_name = trun_rev[::-1]
-        return (new_name)
 
     def getReps(file_name_fun):
         
         with open(file_name_fun,'r') as f:
             #creates new file name for representative sequences, creates, and opens the file
-            file_name_new = getFilename(file_name_fun)
-            file_name_new = directory+"/"+file_name_new+ ".rep.fasta"
+            file_name_new = dante.makeNewFileName ("Desktop/Output", file_name_fun, ".reps.fasta")
             newFile = open(file_name_new,'w')
+            print "\n"
+            print 'Input File:  %s' % (file_name_fun)
+            print 'Output File: %s' % (file_name_new) 
 
             #goes through each line in f
             printsequences = False
@@ -43,14 +35,17 @@ try:
                 #if the line is a new header line, test to see if the genus had been seen before
                 if line[0] == ">":
                     searchObject=re.search(r'\|\s\w*\s', line) #this matches "| genusname " in NCBI FASTA files
-                    genusName = searchObject.group()
-                    #print genusName #used for testing
-                    if genusName not in genusList:
-                        genusList.append(genusName)
-                        printsequences= True
-                        newFile.write(line)
+                    if searchObject == None:  #if re.search finds nothing, it returns None.
+                        printsequences = False  #if it can't find a genus name, it does not print the sequences
                     else:
-                        printsequences = False
+                        genusName = searchObject.group()
+                        #test to see if the genus has already been seen
+                        if genusName not in genusList:
+                            genusList.append(genusName)
+                            printsequences= True
+                            newFile.write(line)
+                        else:
+                            printsequences = False
                     
                 #if the line is a not the start of a new sequence
                 #write the line to the new file if it is a continuation of a previous sequence 
@@ -58,12 +53,6 @@ try:
                     if printsequences == True:
                         newFile.write(line)
             newFile.close()
-
-    
-    #creates the "Output" file on the Desktop if it does not already exist.
-    directory = "Desktop/Output"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
 
     #If filenames were passed at the command line, runs getReps on every file
 
