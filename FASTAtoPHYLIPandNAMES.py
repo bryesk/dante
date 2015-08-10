45ce7a682b2c7113058abea6b1a157ae91ee6ac9
bryesk <Bry@Pelican.local>
2015-08-10 12:30:45 -0400

    FASTA to PHYLIP and NAMES
    Update to streamline and use dante.py module. Found bug that treats
    some, but not all, fasta files as a single line, which breaks
    functionality. Unsure of how to fix.

--- FASTAtoPHYLIPandNAMES.py
+++ FASTAtoPHYLIPandNAMES.py
@@ -3,97 +3,63 @@
 #It then returns a PHYLIP sequential formatted sequence file.  
 #This program allows multiple file names to be called as arguments.
 
-#imports os to create file directories
-import os
-#imports sys to help with file input
-import sys
+#Last updated: 2015.8.10
+#Known bugs: for some files, reads the entire file as one line. Unknown why. 
 
+import sys            #for helping with command line interface
+import dante          #main dante functions
+import traceback      #for helping with errors
 
-#Function for removing the directory of a file name
-def getFilename(whole_name):
-    reversed_name = whole_name[::-1]
-    trun_rev=""
-    for i in range(len(whole_name)):
-        if reversed_name[i] == "/":
-            break
-        else:
-            trun_rev = trun_rev + reversed_name[i]
-    new_name = trun_rev[::-1]
-    return (new_name)
-
-#takes a string and fills up the front until it is 7 characters long. Then appends 'SEQ' to the front. 
-def makeTen(num_string):
-    num_list = list(num_string)
-    while len(num_list) < 7:
-      num_list.insert(0,'0')
-    return('SEQ' + "".join(num_list))  
-        
-def createNames(refFile):
-
-    #NEED TO GO THROUGH ALL THE LINES OF THE REFERENCE FILE AND WRITE THE HEADER LINE TO A NEW LIST
-    with open (refFile, 'r') as f:
-            
-        #opens a new file name for names
-        file_name_names = getFilename(refFile)
-        file_name_names = directory+"/"+file_name_names+ ".dnames"
-        namesFile = open(file_name_names,'w')
-            
-        
-        #open a new file to hold phylip document
-        file_name_phylip = getFilename(refFile)
-        file_name_phylip = directory+"/"+file_name_phylip+ ".phylips"
-        phylipFile = open(file_name_phylip,'w')
-
-        names_number = 0
-
-        #create phylip header (count number of sequences and the number of characters in first sequence)
-        sequence_number = 0
-        character_number = 0
-        first_line_trigger = True
-        second_line_trigger = True
-        for line in f:
-            if line [0] == ">":
-                sequence_number = sequence_number + 1
-            else:
-                if sequence_number == 1:
-                    character_number = character_number + len(''.join(line.split()))
-
-        phylipFile.write(str(sequence_number) + " " + str(character_number))
-
-    with open (refFile, 'r') as f: 
-        for line in f:
-            if line[0] == ">":
-                #write new name to name file and phylip document
-                new_name = makeTen(str(int(names_number)))
-                namesFile.write(new_name + "\n")
-                phylipFile.write("\n" + new_name + " ")
-                names_number = names_number + 1
-                #write this line to name file (minus >)
-                namesFile.write(line.strip('>'))
-            else:
-                phylipFile.write(''.join(line.split()))
-                
-
-    phylipFile.close()
-    namesFile.close()
-                
-    return()
-
-
-#creates the "Output" file on the desktop if it does not already exist
-directory = "Desktop/Output"
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-#MAIN CONTROL BLOCK- ALLOWS FOR CALLING MULTIPLE FILES AS ARGUMENTS
+try:
     
-if len(sys.argv) > 1:  #the first argument in sys.argv is always the script name
-    file_names = sys.argv[1:]
-    #run the changeToFasta command on every file name if other commands are given
-    for filename in file_names:
-        createNames(filename)
+    for file_name in dante.makeFileList(sys.argv):
+        file_name_phylip = dante.makeNewFileName ("Desktop/Output", file_name, ".phylips")
+        file_name_names = dante.makeNewFileName ("Desktop/Output", file_name, ".dnames")
+        dante.printOutput(file_name_phylip)
+        dante.printOutput(file_name_names)
+        
+        with open(file_name,'r') as f: 
+            
+            #NEED TO GO THROUGH ALL THE LINES OF THE REFERENCE FILE AND WRITE THE HEADER LINE TO A NEW LIST
+            
+            #create phylip header (count number of sequences and the number of characters in first sequence)
+            sequence_number = 0
+            character_number = 0
+            
+            for line in f:
+                if line [0] == ">":
+                    sequence_number = sequence_number + 1
+                else:
+                    if sequence_number == 1:
+                        character_number = character_number + len(''.join(line.split()))
 
-else:
-    file_name = str(raw_input('Enter the file to be changed: '))
-    createNames(file_name)
+            
+        with open(file_name,'r') as f, open(file_name_phylip, 'w') as g, open(file_name_names,'w') as h:
+            #Write phylip header (contains number of sequences and the number of characters in each sequence
+            g.write(str(sequence_number) + " " + str(character_number))
+            #initial sequence name
+            new_name = 'SEQ0000000'
+            names_number = 0
+            
+            for line in f:
+                if line[0] == ">":
+                    #write new name to name file and phylip document                    
+                    h.write(new_name + "\n")
+                    g.write("\n" + new_name + " ")
+                    
+                    #write this line to name file (minus >)
+                    h.write(line.strip('>'))
+
+                    #set up next name
+                    names_number = names_number + 1
+                    new_name = new_name[:-len(str(int(names_number)))] + str(names_number)
+                    
+                else:
+                    g.write(''.join(line.split()))
+            
+except:
+    traceback.print_exc(file=sys.stdout)
+    exit(0)
+
+#test python link/dante/FASTAtoPHYLIPandNAMES.py link/seqs/mothur/MFArc_Files/MFArc.unique.fasta
 
